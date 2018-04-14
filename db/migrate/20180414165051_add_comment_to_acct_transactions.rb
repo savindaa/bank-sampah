a0c8a2f3350cd32660e9fe46c3de2cc35cd153ec
thekingisme <ks.dwiputra@yahoo.com>
2018-04-15 00:55:56 +0700

    comment

--- db/migrate/20180414165051_add_comment_to_acct_transactions.rb
+++ db/migrate/20180414165051_add_comment_to_acct_transactions.rb
@@ -1,11 +1,11 @@
 class AddCommentToAcctTransactions < ActiveRecord::Migration[5.1]
   def change
-    # remove_index :acct_transactions, :approved
-    # remove_column :acct_transactions, :approved, :boolean
-    # remove_index :acct_transactions, :showed
-    # remove_column :acct_transactions, :showed, :boolean
-    # add_column :acct_transactions, :status, :string, default: "1"
-    # add_index :acct_transactions, :status
+    remove_index :acct_transactions, :approved
+    remove_column :acct_transactions, :approved, :boolean
+    remove_index :acct_transactions, :showed
+    remove_column :acct_transactions, :showed, :boolean
+    add_column :acct_transactions, :status, :string, default: "1"
+    add_index :acct_transactions, :status
     add_column :acct_transactions, :comment, :string
   end
 end
