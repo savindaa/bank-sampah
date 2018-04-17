4455a727d0143f1387053040df087f30982e5be1
thekingisme <ks.dwiputra@yahoo.com>
2018-04-17 19:17:45 +0700

    Cant fix datetime heroku

--- config/initializers/time_zone_changer.rb
+++ config/initializers/time_zone_changer.rb
@@ -1,12 +1,12 @@
 class ActiveSupport::TimeWithZone
     def as_json(options = {})
         @namahari = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"]
-        @hari = @namahari[to_datetime.strftime("%a").to_i]
+        @hari = @namahari[strftime("%a").to_i]
         @tanggal = strftime("%d").to_i
         @namabulan = ["Januari", "February", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
-        @bulan = @namabulan[to_datetime.strftime("%m").to_i]
-        @tahun = to_datetime.strftime("%Y")
-        @waktu = to_datetime.strftime("%H:%M %Z")
+        @bulan = @namabulan[strftime("%m").to_i]
+        @tahun = strftime("%Y")
+        @waktu = strftime("%H:%M %Z")
         "#{@hari}, #{@tanggal} #{@bulan} #{@tahun}, pukul #{@waktu}"
     end
 end
