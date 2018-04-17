55a9346756fa904a45a5edbd373f18d77bb5f64e
thekingisme <ks.dwiputra@yahoo.com>
2018-04-17 20:32:45 +0700

    m

--- app/serializers/v1/pick_request_serializer.rb
+++ app/serializers/v1/pick_request_serializer.rb
@@ -6,24 +6,49 @@ class V1::PickRequestSerializer < ActiveModel::Serializer
   has_many :trash_details
 
   def created_at
-    @namahari = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"]
-    @hari = @namahari[object.created_at.in_time_zone('Jakarta').strftime("%a").to_i]
-    @tanggal = object.created_at.in_time_zone('Jakarta').strftime("%d").to_i
-    @namabulan = ["Januari", "February", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
-    @bulan = @namabulan[object.created_at.in_time_zone('Jakarta').strftime("%m").to_i]
-    @tahun = object.created_at.in_time_zone('Jakarta').strftime("%Y")
-    @waktu = object.created_at.in_time_zone('Jakarta').strftime("%H:%M %Z")
+    @day = object.created_at.strftime("%A")
+    day_changer(@day)
+    @tanggal = object.created_at.strftime("%d").to_i
+    @namabulan = ["", "Januari", "February", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
+    @bulan = @namabulan[object.created_at.strftime("%m").to_i]
+    @tahun = object.created_at.strftime("%Y")
+    @waktu = object.created_at.strftime("%H:%M %Z")
     "#{@hari}, #{@tanggal} #{@bulan} #{@tahun}, pukul #{@waktu}"
   end
 
   def confirmed_at
-    @namahari = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"]
-    @hari = @namahari[object.updated_at.in_time_zone('Jakarta').strftime("%a").to_i]
-    @tanggal = object.updated_at.in_time_zone('Jakarta').strftime("%d").to_i
-    @namabulan = ["Januari", "February", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
-    @bulan = @namabulan[object.updated_at.in_time_zone('Jakarta').strftime("%m").to_i]
-    @tahun = object.updated_at.in_time_zone('Jakarta').strftime("%Y")
-    @waktu = object.updated_at.in_time_zone('Jakarta').strftime("%H:%M %Z")
+    @day = object.updated_at.strftime("%A")
+    day_changer(@day)
+    @tanggal = object.updated_at.strftime("%d").to_i
+    @namabulan = ["", "Januari", "February", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
+    @bulan = @namabulan[object.updated_at.strftime("%m").to_i]
+    @tahun = object.updated_at.strftime("%Y")
+    @waktu = object.updated_at.strftime("%H:%M %Z")
     "#{@hari}, #{@tanggal} #{@bulan} #{@tahun}, pukul #{@waktu}"
+  end
+
+  def day_changer(day)
+    case day
+        when "Sunday"
+            @hari = "Minggu"
+        
+        when "Monday"
+            @hari = "Senin"
+
+        when "Tuesday"
+            @hari = "Selasa"
+
+        when "Wednesday"
+            @hari = "Rabu"
+
+        when "Thursday"
+            @hari = "Kamis"
+
+        when "Friday"
+            @hari = "Jumat"
+
+        when "Saturday"
+            @hari = "Sabtu"
+    end
   end
 end
--- config/initializers/time_zone_changer.rb
+++ config/initializers/time_zone_changer.rb
@@ -1,23 +1,44 @@
 class ActiveSupport::TimeWithZone
-    def as_json(options = {})
-        @namahari = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"]
-        @hari = @namahari[in_time_zone('Jakarta').strftime("%a").to_i]
-        @tanggal = in_time_zone('Jakarta').strftime("%d").to_i
-        @namabulan = ["Januari", "February", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
-        @bulan = @namabulan[in_time_zone('Jakarta').strftime("%m").to_i]
-        @tahun = in_time_zone('Jakarta').strftime("%Y")
-        @waktu = in_time_zone('Jakarta').strftime("%H:%M %Z")
-        "#{@hari}, #{@tanggal} #{@bulan} #{@tahun}, pukul #{@waktu}"
-    end
-
     # def as_json(options = {})
     #     @namahari = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"]
-    #     @hari = @namahari[strftime("%a").to_i]
-    #     @tanggal = strftime("%d").to_i
+    #     @hari = @namahari[in_time_zone('Jakarta').strftime("%a").to_i]
+    #     @tanggal = in_time_zone('Jakarta').strftime("%d").to_i
     #     @namabulan = ["Januari", "February", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
-    #     @bulan = @namabulan[strftime("%m").to_i]
-    #     @tahun = strftime("%Y")
-    #     @waktu = strftime("%H:%M %Z")
+    #     @bulan = @namabulan[in_time_zone('Jakarta').strftime("%m").to_i]
+    #     @tahun = in_time_zone('Jakarta').strftime("%Y")
+    #     @waktu = in_time_zone('Jakarta').strftime("%H:%M %Z")
     #     "#{@hari}, #{@tanggal} #{@bulan} #{@tahun}, pukul #{@waktu}"
     # end
+
+    def as_json(options = {})
+        @day = strftime("%A")
+        case @day
+            when "Sunday"
+                @hari = "Minggu"
+            
+            when "Monday"
+                @hari = "Senin"
+
+            when "Tuesday"
+                @hari = "Selasa"
+
+            when "Wednesday"
+                @hari = "Rabu"
+
+            when "Thursday"
+                @hari = "Kamis"
+
+            when "Friday"
+                @hari = "Jumat"
+
+            when "Saturday"
+                @hari = "Sabtu"
+        end
+        @tanggal = strftime("%d").to_i
+        @namabulan = ["", "Januari", "February", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
+        @bulan = @namabulan[strftime("%m").to_i]
+        @tahun = strftime("%Y")
+        @waktu = strftime("%H:%M %Z")
+        "#{@hari}, #{@tanggal} #{@bulan} #{@tahun}, pukul #{@waktu}"
+    end
 end
